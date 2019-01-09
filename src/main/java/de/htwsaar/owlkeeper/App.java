f74d61540cb8395c6a27df9b1ea1a5d79e6275ef
Matthias Riegler <me@xvzf.tech>
2019-01-09 03:14:17 +0100

    Maven project structure, JUNIT5, Hibernate

---
+++ src/main/java/de/htwsaar/owlkeeper/App.java
@@ -0,0 +1,13 @@
+package de.htwsaar.owlkeeper;
+
+/**
+ * Owlkeeper
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Owlkeeper" );
+    }
+}
---
+++ src/test/java/de/htwsaar/owlkeeper/AppTest.java
@@ -0,0 +1,19 @@
+package de.htwsaar.owlkeeper;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
