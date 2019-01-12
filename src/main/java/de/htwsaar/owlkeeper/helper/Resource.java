88de8b718fe0edff01cdcf6d6e4e5d6198b2b92c
Matthias Riegler <me@xvzf.tech>
2019-01-12 12:58:19 +0100

    Added logging

--- src/main/java/de/htwsaar/owlkeeper/helper/Resource.java
+++ src/main/java/de/htwsaar/owlkeeper/helper/Resource.java
@@ -6,21 +6,35 @@ import java.io.InputStream;
 import java.nio.charset.Charset;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
- * Resource
+ * Resource helper class
  */
 public class Resource {
-    public static String getClassResourceAsString(Object o, String name) throws ResourceNotFoundException {
-        InputStream inputStream = o.getClass().getResourceAsStream(name);
+    private static Logger logger = LogManager.getLogger(Resource.class);
+
+    /**
+     * Loads a resource into a string
+     *
+     * @param o Current object
+     * @param path Path to the resource
+     * @return String representation of the resource
+     * @throws ResourceNotFoundException Resource could not be accessed
+     */
+    public static String getResourceAsString(Object o, String path) throws ResourceNotFoundException {
+        InputStream inputStream = o.getClass().getResourceAsStream(path);
 
         if(inputStream == null) {
+            logger.error("Could not open resource @ " + path);
             throw new ResourceNotFoundException();
         }
 
         try {
             return IOUtils.toString(inputStream, Charset.forName("UTF-8"));
         } catch (IOException ie) {
+            logger.error("Could not read resource @ " + path, ie);
             throw new ResourceNotFoundException();
         }
     }
