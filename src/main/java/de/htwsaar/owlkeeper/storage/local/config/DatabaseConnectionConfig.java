42112dfc6c985596896c925949d75b8cd421a6cd
Mark Martinssen <martinussen.mark@gmail.com>
2019-01-15 19:01:21 +0100

    Expanded config implementation, made editable.
    Edits using factory methods so that the process that wishes to edit the configuration does not have to access the configuration owner but can simply access the ConfigEditor.

--- src/main/java/de/htwsaar/owlkeeper/storage/local/config/DatabaseConnectionConfig.java
+++ src/main/java/de/htwsaar/owlkeeper/storage/local/config/DatabaseConnectionConfig.java
@@ -1,5 +1,7 @@
 package de.htwsaar.owlkeeper.storage.local.config;
 
+import de.htwsaar.owlkeeper.helper.constants.DatabaseConstants.DATABASE_CONFIGS;
+
 import java.util.Properties;
 import java.util.prefs.Preferences;
 
@@ -9,34 +11,52 @@ import java.util.prefs.Preferences;
 public class DatabaseConnectionConfig implements Config {
     private final Preferences preferences;
 
-    public DatabaseConnectionConfig(Preferences preferences) {
+    public DatabaseConnectionConfig(final Preferences preferences) {
         this.preferences = preferences;
     }
 
     public String getUrl() {
-        return preferences.get("url", null);
+        return preferences.get(DATABASE_CONFIGS.URL.toString(), null);
+    }
+
+    public void changeUrl(final String url) {
+        preferences.put(DATABASE_CONFIGS.URL.toString(), url);
     }
 
     public String getUser() {
-        return preferences.get("user", null);
+        return preferences.get(DATABASE_CONFIGS.USER.toString(), null);
+    }
+
+    public void changeUser(final String user) {
+        preferences.put(DATABASE_CONFIGS.USER.toString(), user);
     }
 
     public String getPassword() {
-        return preferences.get("password", null);
+        return preferences.get(DATABASE_CONFIGS.PASSWORD.toString(), null);
+    }
+
+    public void changePassword(final String password) {
+        preferences.put(DATABASE_CONFIGS.PASSWORD.toString(), password);
     }
 
     public String getUse_ssl() {
-        return preferences.get("use_ssl", null);
+        return preferences.get(DATABASE_CONFIGS.USE_SSL.toString(), null);
+    }
+
+    public void changeUse_ssl(final String flag) {
+        if (flag.equals("true") || flag.equals("false")) {
+            preferences.put(DATABASE_CONFIGS.USE_SSL.toString(), flag);
+        }
     }
 
     private Preferences getPreferences() {
         return preferences;
     }
 
-
     /**
-     * Turns the Preferences into a Java Properties Map to enable conection to the database.
-     * @return
+     * Turns the Preferences into a Java Properties Map to enable connection to the database.
+     *
+     * @return A Properties Object containing connection information.
      */
     public Properties connectionProperties() {
         Properties props = new Properties();
