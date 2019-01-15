7a8a873678fcd7a710bb1b9c7a28a9f23419beca
Mark Martinssen <martinussen.mark@gmail.com>
2019-01-15 12:12:51 +0100

    Added minimal constructors to storage.db.DBTransactionException.

--- src/main/java/de/htwsaar/owlkeeper/storage/db/DBTransactionException.java
+++ src/main/java/de/htwsaar/owlkeeper/storage/db/DBTransactionException.java
@@ -3,4 +3,11 @@ package de.htwsaar.owlkeeper.storage.db;
 import java.sql.SQLException;
 
 public class DBTransactionException extends SQLException {
+    public DBTransactionException() {
+        super();
+    }
+
+    public DBTransactionException(String s) {
+        super(s);
+    }
 }
