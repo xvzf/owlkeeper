93761a19a37ea27e0fde183760dfb27ca3e626b9
Mark Martinssen <martinussen.mark@gmail.com>
2019-01-15 12:06:03 +0100

    Added minimal constructors to helper.ResourceNotFoundException.

--- src/main/java/de/htwsaar/owlkeeper/helper/ResourceNotFoundException.java
+++ src/main/java/de/htwsaar/owlkeeper/helper/ResourceNotFoundException.java
@@ -6,5 +6,11 @@ import java.io.FileNotFoundException;
  * ResourceNotFoundException
  */
 public class ResourceNotFoundException extends FileNotFoundException {
+    public ResourceNotFoundException() {
+        super();
+    }
 
+    public ResourceNotFoundException(String s) {
+        super(s);
+    }
 }
