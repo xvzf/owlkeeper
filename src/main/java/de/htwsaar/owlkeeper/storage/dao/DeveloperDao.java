fc23726f5df264bf2fafd6678b34be9f40a89045
Matthias Riegler <me@xvzf.tech>
2019-01-21 19:38:51 +0100

    Moving from jdbc over to jdbi for easier SQL handling; Team and Developer Objects implemented

---
+++ src/main/java/de/htwsaar/owlkeeper/storage/dao/DeveloperDao.java
@@ -0,0 +1,20 @@
+package de.htwsaar.owlkeeper.storage.dao;
+
+import de.htwsaar.owlkeeper.storage.entity.Developer;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+
+import java.util.List;
+
+public interface DeveloperDao {
+
+    /**
+     * Get all developers
+     *
+     * @return List of developers
+     */
+    @SqlQuery("select * from developer;")
+    @RegisterBeanMapper(Developer.class)
+    List<Developer> getDeveloper();
+}
