9f8702466b9ab2c9469cb0d21f9b047d5551d2c6
Matthias Riegler <me@xvzf.tech>
2019-01-23 11:05:06 +0100

    Entity container for all relevant DB tables

---
+++ src/main/java/de/htwsaar/owlkeeper/storage/entity/Project.java
@@ -0,0 +1,62 @@
+package de.htwsaar.owlkeeper.storage.entity;
+
+import java.sql.Timestamp;
+
+public class Project {
+    long id;
+    Timestamp created;
+    String name;
+    String description;
+    String type;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", created=" + created +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
