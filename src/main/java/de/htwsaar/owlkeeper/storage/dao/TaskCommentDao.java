4b769a9d39ca37b5b9c31aabb4de0c963e9101b8
Matthias Riegler <me@xvzf.tech>
2019-01-23 12:32:50 +0100

    TaskComment Dao

---
+++ src/main/java/de/htwsaar/owlkeeper/storage/dao/TaskCommentDao.java
@@ -0,0 +1,29 @@
+package de.htwsaar.owlkeeper.storage.dao;
+
+import de.htwsaar.owlkeeper.storage.entity.Developer;
+import de.htwsaar.owlkeeper.storage.entity.TaskComment;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+
+import java.util.List;
+
+public interface TaskCommentDao {
+
+    /**
+     * Query task comment by id
+     *
+     */
+    @SqlQuery("select * from task_comment where id = ?;")
+    @RegisterBeanMapper(TaskComment.class)
+    TaskComment getComment(long id);
+
+    /**
+     * Queries comments for task
+     *
+     * @param id
+     * @return
+     */
+    @SqlQuery("select * from task_comment where task_comment.task = ?;")
+    @RegisterBeanMapper(TaskComment.class)
+    List<TaskComment> getCommentsForTask(long id);
+}
