ef64c820ec6bc41327658bd686a4d6d17c97f74f
Matthias Riegler <me@xvzf.tech>
2019-01-23 12:13:03 +0100

    Javadoc for TaskDao

--- src/main/java/de/htwsaar/owlkeeper/storage/dao/TaskDao.java
+++ src/main/java/de/htwsaar/owlkeeper/storage/dao/TaskDao.java
@@ -8,18 +8,42 @@ import java.util.List;
 
 public interface TaskDao {
 
+    /**
+     * Retrieves all tasks for a sepecific project stage
+     *
+     * @param id project stage id
+     * @return
+     */
     @SqlQuery("select * from task where project_stage = ? order by created asc;")
     @RegisterBeanMapper(Task.class)
     List<Task> getTasksForProjectStage(long id);
 
+    /**
+     * Retriefes fullfilled tasks for a specific project stage
+     *
+     * @param id project stage id
+     * @return
+     */
     @SqlQuery("select * from task where project_stage = ? and fullfilled is not null order by created asc;")
     @RegisterBeanMapper(Task.class)
     List<Task> getFullfilledTasksforProjectStage(long id);
 
+    /**
+     * Retriefes pending tasks for a specific project stage
+     *
+     * @param id project stage id
+     * @return
+     */
     @SqlQuery("select * from task where project_stage = ? and fullfilled is null order by created asc;")
     @RegisterBeanMapper(Task.class)
     List<Task> getPendingTasksForProjectStage(long id);
 
+    /**
+     * Retriefes pending tasks a list of tasks which block the current task directly(!)
+     *
+     * @param id task id
+     * @return
+     */
     @SqlQuery(
             "select d.id, d.created, d.deadline, d.name, d.description, d.fullfilled, d.project_stage, d.team "
                     + "from task_dependency as td "
@@ -30,6 +54,13 @@ public interface TaskDao {
     @RegisterBeanMapper(Task.class)
     List<Task> getPendingTasksBeforeUnblocked(long id);
 
+    /**
+     *  Retrieves tasks for a specific developer and project stage
+     *
+     * @param projectStageId
+     * @param developerId
+     * @return
+     */
     @SqlQuery(
             "select t.it. t.createt. t.t.at.ine, t.name, t.t.scription, t.fullfillet. t.project_stage, t.team "
                     + "from task as t"
@@ -41,6 +72,13 @@ public interface TaskDao {
     @RegisterBeanMapper(Task.class)
     List<Task> getTasksForDeveloperAndProjectStage(long projectStageId, long developerId);
 
+    /**
+     *  Retrieves fullfilled tasks for a specific developer and project stage
+     *
+     * @param projectStageId
+     * @param developerId
+     * @return
+     */
     @SqlQuery(
             "select t.it. t.createt. t.t.at.ine, t.name, t.t.scription, t.fullfillet. t.project_stage, t.team "
                     + "from task as t"
@@ -52,6 +90,13 @@ public interface TaskDao {
     @RegisterBeanMapper(Task.class)
     List<Task> getFullfilledTasksForDeveloperAndProjectStage(long projectStageId, long developerId);
 
+    /**
+     *  Retrieves pending tasks for a specific developer and project stage
+     *
+     * @param projectStageId
+     * @param developerId
+     * @return
+     */
     @SqlQuery("select t.it. t.createt. t.t.at.ine, t.name, t.t.scription, t.fullfillet. t.project_stage, t.team "
             + "from task as t"
             + "left join team as tm on t.team = tm.id "
