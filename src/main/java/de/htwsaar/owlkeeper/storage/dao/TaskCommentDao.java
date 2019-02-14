e0a07370e16e2cfd5433b021053e0f4bfad3e011
Patrick Plewka <patrickplewka@googlemail.com>
2019-02-14 19:31:34 +0100

    Basic TaskModel TaskCommentModel NO TESTS

--- src/main/java/de/htwsaar/owlkeeper/storage/dao/TaskCommentDao.java
+++ src/main/java/de/htwsaar/owlkeeper/storage/dao/TaskCommentDao.java
@@ -2,6 +2,7 @@ package de.htwsaar.owlkeeper.storage.dao;
 
 import de.htwsaar.owlkeeper.storage.entity.TaskComment;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
 import java.util.List;
@@ -25,4 +26,38 @@ public interface TaskCommentDao {
     @SqlQuery("select * from task_comment where task_comment.task = ?;")
     @RegisterBeanMapper(TaskComment.class)
     List<TaskComment> getCommentsForTask(long id);
+
+    /**
+     * Inserts a new TaskComment into the database
+     *
+     * @param p
+     * @return Inserted id
+     */
+    @SqlQuery("insert into task_comment (name, description, type) values (:name, :description, :type) returning id;"
+    )
+    int insertTaskComment(@BindBean TaskComment p);
+
+    /**
+     * Updates an existing task comment
+     *
+     * @param p
+     * @return
+     */
+    @SqlQuery("update task_comment set "
+            + "content = :content"
+            + ", developer = :developer"
+            + ", task = :task"
+            + " where id = :id returning id;"
+    )
+    int updateTaskComment(@BindBean TaskComment p);
+
+    /**
+     * Removes a task comment from the database
+     *
+     * @param id
+     * @return Removed id
+     */
+    @SqlQuery("delete from task_comment where id = ? returning id;")
+    @RegisterBeanMapper(TaskComment.class)
+    int deleteTaskComment(long id);
 }
