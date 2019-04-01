bac987fb93bfe4319281b6a08e34e6cd5bec7138
Mark Martinussen <martinussen.mark@gmail.com>
2019-04-01 15:43:21 +0200

    Implemented static permission checking

--- src/main/java/de/htwsaar/owlkeeper/helper/Permissions.java
+++ src/main/java/de/htwsaar/owlkeeper/helper/Permissions.java
@@ -2,15 +2,37 @@ package de.htwsaar.owlkeeper.helper;
 
 public enum Permissions {
 
-    CREATE_PROJECT(1), DELETE_PROJECT(2),
-    CREATE_TASK(4), VIEW_TASK(8),
-    FULFIL_TASK(16), ASSIGN_TEAM_TO_TASK(32),
-    CREATE_USER(64), CREATE_TEAM(128), ADD_DEVELOPER_TO_TEAM(256);
+    CREATE_PROJECT(1), DELETE_PROJECT(2), CREATE_PROJECT_STAGE(4), DELETE_PROJECT_STAGE(8),
+    CREATE_TASK(16), VIEW_TASK(32),
+    FULFIL_TASK(64), ASSIGN_TEAM_TO_TASK(128),
+    CREATE_USER(256), CREATE_TEAM(512), DISSOLVE_TEAM(1024), //TODO More static permissions
+
+    PROJECT_OWNER_PERMISSIONS(CREATE_PROJECT.get() + DELETE_PROJECT.get() + CREATE_PROJECT_STAGE.get() + DELETE_PROJECT_STAGE.get() + ASSIGN_TEAM_TO_TASK.get() +
+            CREATE_USER.get() + CREATE_TEAM.get() + DISSOLVE_TEAM.get()),
+
+    DEVELOPER_PERMISSIONS(CREATE_TASK.get() + VIEW_TASK.get() + CREATE_USER.get());
 
     private int val;
 
     Permissions(int val) {
         this.val = val;
+    }
+
+    /**
+     * Retrieves the static permissions for a given group
+     *
+     * @param group the group to retrieve the permissions for.
+     * @return
+     */
+    public static int permissionOf(final String group) {
+        if (group.equals("project")) {
+            return PROJECT_OWNER_PERMISSIONS.get();
+        } else if (group.equals("task")) {
+            return DEVELOPER_PERMISSIONS.get();
+        } else {
+            return 0;
+        }
+
     }
 
     public int get() {
--- src/main/java/de/htwsaar/owlkeeper/service/PermissionHandler.java
+++ src/main/java/de/htwsaar/owlkeeper/service/PermissionHandler.java
@@ -1,8 +1,10 @@
 package de.htwsaar.owlkeeper.service;
 
 import de.htwsaar.owlkeeper.helper.DeveloperManager;
+import de.htwsaar.owlkeeper.helper.Permissions;
 import de.htwsaar.owlkeeper.helper.exceptions.InsufficientPermissionsException;
 import de.htwsaar.owlkeeper.helper.exceptions.UserInitializationException;
+import de.htwsaar.owlkeeper.storage.entity.Developer;
 import de.htwsaar.owlkeeper.storage.entity.HasID;
 import de.htwsaar.owlkeeper.storage.model.DeveloperModel;
 
@@ -26,6 +28,13 @@ public class PermissionHandler {
         return handler;
     }
 
+    /**
+     * Check a static permission described in enum {@link de.htwsaar.owlkeeper.helper.Permissions}, each group is given
+     * a set of statically defined permissions.
+     *
+     * @param action the permission to check
+     * @throws InsufficientPermissionsException If the user does not have the requested permission.
+     */
     public static boolean checkPermission(final int action) throws InsufficientPermissionsException {
         if (userGroup.equals("admin")) return true;
         if (checkAction(userGroup, action)) {
@@ -38,13 +47,13 @@ public class PermissionHandler {
 
     /**
      * Check a dynamic permission described by a SQL Query in {@link de.htwsaar.owlkeeper.storage.dao.AccessControlDao}
-     * or any other conditional.
+     * or any other conditional using the currently logged in developer to check permissions.
      *
-     * @param predicate The conditional to check as a Predicate. The parameter of this predicate can be any {@link de.htwsaar.owlkeeper.storage.entity}
-     *                  (Most commonly the current user, which the PermissionHandler knows)
-     * @throws InsufficientPermissionsException If the user/group does not have the requested permission.
+     * @param predicate The conditional to check as a Predicate. The parameter of this predicate is interpreted by the PermissionHandler
+     *                  as the currently authenticated user.
+     * @throws InsufficientPermissionsException If the user does not have the requested permission.
      */
-    public static boolean checkPermission(final Predicate<? super HasID> predicate) throws InsufficientPermissionsException {
+    public static boolean checkPermission(final Predicate<Developer> predicate) throws InsufficientPermissionsException {
         if (userGroup.equals("admin")) return true;
         if (predicate.test(user.getContainer())) {
             return true;
@@ -55,8 +64,9 @@ public class PermissionHandler {
     }
 
     private static boolean checkAction(final String group, final int action) {
-        //TODO
-        return false;
+        int permissions = Permissions.permissionOf(group);
+        permissions >>>= (int) (Math.log(action) / Math.log(2));
+        return permissions % 2 == 0;
     }
 
 }
