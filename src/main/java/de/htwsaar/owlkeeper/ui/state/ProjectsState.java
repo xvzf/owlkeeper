10d1cf0c8fb15e8d85c3facb092f46379c2be78e
Jannik Schäfer <me@jschaefer.io>
2019-04-04 23:09:31 +0200

    remove unneeded state-code

--- src/main/java/de/htwsaar/owlkeeper/ui/state/ProjectsState.java
+++ src/main/java/de/htwsaar/owlkeeper/ui/state/ProjectsState.java
@@ -1,16 +1,3 @@
 package de.htwsaar.owlkeeper.ui.state;
 
-import java.util.HashMap;
-
-public class ProjectsState extends BaseState{
-    public void handleQuery(HashMap<String, Object> query){
-        super.handleQuery(query);
-    }
-
-    @Override
-    public HashMap<String, Object> collectState(){
-        HashMap<String, Object> output = super.collectState();
-        System.out.println(output);
-        return output;
-    }
-}
+public class ProjectsState extends BaseState{}
